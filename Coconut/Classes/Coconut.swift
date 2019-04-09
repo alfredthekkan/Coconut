59e9139bb879193a88e5174d94c61863f86bf8d6
Alfred <alfred@farabi.ae>
2019-04-09 10:38:33 +0400

    Second commit

---
+++ Coconut/Classes/Coconut.swift
@@ -0,0 +1,20 @@
+//
+//  Coconut.swift
+//  Coconut
+//
+//  Created by Alfred Thekkan on 4/9/19.
+//
+
+import Foundation
+
+public class Coconut {
+    
+    public class var version: String {
+        return "1.0.0"
+    }
+    
+    public class var author: String {
+        return "Alfred Thekkan"
+    }
+    
+}
